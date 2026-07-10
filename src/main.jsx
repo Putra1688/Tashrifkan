5ec52c8fb00ef556b920ab045dcade530979bb83
Putra1688 <ranggadsaputra11@gmail.com>
2026-07-10 10:49:48 +0700

    feat:integrated into PWA Architecture

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { registerSW } from 'vite-plugin-pwa'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  registerSW({ immediate: true })
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate', // Otomatis update cache jika ada perubahan kode
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Tashrifkan - Belajar Shorof Interaktif',
+        short_name: 'Tashrifkan',
+        description: 'Media pembelajaran interaktif ilmu Shorof dan konsep Mabni berbasis game.',
+        theme_color: '#0f172a', // Menyesuaikan bg-slate-950/900
+        background_color: '#0f172a',
+        display: 'standalone',
+        orientation: 'landscape', // <-- Mengunci orientasi aplikasi secara sistem ke Landscape!
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable' // Supaya ikon pas di berbagai bentuk UI HP
+          }
+        ]
+      }
+    })
+  ],
 })
